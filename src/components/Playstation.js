8b52b59c2f4bcf51b51ade265c2102e656a827e6
Ionut Ianchis <i.ionutz50@gmail.com>
2021-10-12 16:18:54 +0300

    Added backend functionality for high scores and character position

--- src/components/Playstation.js
+++ src/components/Playstation.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-unused-vars */
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useImperativeHandle, useReducer, useState} from 'react'
 import Modal from 'react-modal'
 import { useHistory } from 'react-router-dom'
+import Stopwatch from './Stopwatch'
 import { initializeApp } from "firebase/app"
 import { getFirestore, collection, doc, getDocs, setDoc} from "firebase/firestore"
 import '../styles/Playstation.css'
@@ -9,8 +10,9 @@ import '../styles/Playstation.css'
 Modal.setAppElement('#root')
 
 const Playstation = () => {
+    
     const history = useHistory();
-
+    
     const [characters, setCharacters] = useState([
         {
             name: 'Tommy',
@@ -28,7 +30,11 @@ const Playstation = () => {
             found: false
         }
     ])
+    const [users, setUsers] = useState([])
     
+    const [time, setTime] = useState(0)
+    const [stopTimer, setStopTimer] = useState(false)
+
     const [dropdown, setDropdown] = useState(false)
 
     const [yPos, setYPos] = useState()
@@ -36,25 +42,52 @@ const Playstation = () => {
     
     const [characterSelection, setCharacterSelection] = useState()
 
+    const [characterPosition, setCharacterPosition] = useState([])
+
+    const [modalOpen, setModalOpen] = useState(false)
+    const [input, setInput] = useState('')
+
+    useEffect(() => {
+        getUsers()
+        getCharacterPosition()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    const getCharacterPosition = async () => {
+        const querySnapshot = await getDocs(collection(db, 'data'))
+        querySnapshot.forEach((doc) => {
+            setCharacterPosition(prevState => ([...prevState,
+            {
+                name: doc.id,
+                xStart: doc.data().xStart,
+                xEnd: doc.data().xEnd,
+                yStart: doc.data().yStart,
+                yEnd: doc.data().yEnd
+            }
+            ]))
+        })
+    }
+
     const ulClick = (e) => {
         setCharacterSelection(e.target.textContent)
-
     }
-
+    // check for correct click
     const checkForGuess = () => {
-        if (xPos > 1092 && xPos < 1233 && yPos > 1885 && yPos < 2030 && characterSelection === 'Tommy') {
-            setCharacters([...characters], characters[0].found = true)
-            setDropdown(!dropdown)
-        } else if (xPos > 290 && xPos < 427 && yPos > 1887 && yPos < 2033 && characterSelection === 'Kratos') {
-            setCharacters([...characters], characters[1].found = true)
-            setDropdown(!dropdown)
-        } else if (xPos > 391 && xPos < 535 && yPos > 2230 && yPos < 2368 && characterSelection === 'Dante') {
+        if (characterSelection !== undefined) {
+            if (xPos > characterPosition[2].xStart && xPos < characterPosition[2].xEnd && yPos > characterPosition[2].yStart && yPos < characterPosition[2].yEnd && characterSelection === 'Tommy') {
+                setCharacters([...characters], characters[0].found = true)
+                setDropdown(!dropdown)
+            } else if (xPos > characterPosition[1].xStart && xPos < characterPosition[1].xEnd && yPos > characterPosition[1].yStart && yPos < characterPosition[1].yEnd && characterSelection === 'Kratos') {
+                setCharacters([...characters], characters[1].found = true)
+                setDropdown(!dropdown)
+            } else if (xPos > characterPosition[0].xStart && xPos < characterPosition[0].xEnd && yPos > characterPosition[0].yStart && yPos < characterPosition[0].yEnd && characterSelection === 'Dante') {
             setCharacters([...characters], characters[2].found = true)
             setDropdown(!dropdown)
+            }
         }
-
     }
 
+    // click coordinates and dropdown menu
     const handleClick = (e) => {
         setYPos(e.pageY)
         setXPos(e.pageX)
@@ -62,11 +95,28 @@ const Playstation = () => {
         setDropdown(!dropdown)
     }
 
-    const [modalOpen, setModalOpen] = useState(false)
+
+    // user name input
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        setUsers(prevState => ([...prevState,
+            {
+                name: input,
+                time: time,
+            }
+        ]))
+
+        addUser()
+        
+    }
+    
 
     const checkForGameOver = () => {
+        // checks for no characters left
         if (characters.every(x => x.found)) {
             return (
+                setStopTimer(true),
                 setModalOpen(true)
                 )
             }
@@ -77,7 +127,12 @@ const Playstation = () => {
         checkForGameOver()
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [characterSelection])
-        
+    
+    const getSeconds = `0${(time % 60)}`.slice(-2)  // formatting for time display
+    const minutes = `${Math.floor(time / 60)}`
+    const getMinutes = `0${minutes % 60}`.slice(-2)
+    
+    // database
     const firebaseApp = initializeApp({
         apiKey: "AIzaSyCc6qLSfNbwMDJz_Gw8O1xxrrTD-CfloF0",
         authDomain: "where-is-waldo-48ef7.firebaseapp.com",
@@ -88,6 +143,33 @@ const Playstation = () => {
     })
     
     const db = getFirestore()
+
+    // get users from database
+    const getUsers = async () => {
+        const querySnapshot = await getDocs(collection(db, 'users'))
+        querySnapshot.forEach((doc) => {
+            setUsers(prevState => ([...prevState,
+                {
+                    name: doc.id,
+                    time: doc.data().time,
+                }
+            ]))
+        })
+    }
+
+
+    // add user to database
+    const addUser = async () => {
+        try {
+            await setDoc(doc(db, 'users', input), {
+                time: time,
+            })
+        } catch (error) {
+            console.error('Error adding document: ', error)
+        }
+    }
+
+    const sortedUsers = users.sort((a, b) => parseFloat(a.time) - parseFloat(b.time)); // sorting users by time
 
     return (
         <div className='playstation-container'>
@@ -111,6 +193,9 @@ const Playstation = () => {
             <div className='playstation-info-container'>
                 <div className='sticky-info'>
                     <button className='back-button' onClick={() => history.push("/where-is-waldo/")}></button>
+
+                    <Stopwatch setTime={setTime} stopTimer={stopTimer} />
+                    
                     {
                         characters.map((char, index) => {
                             let divClass = char.found ? 'found-character-info' : 'character-info'
@@ -134,20 +219,38 @@ const Playstation = () => {
                     overlayClassName="overlay"
                     >
                         <button type='button' onClick={() => setModalOpen(false)} className='modal-close-button'></button>
-
+                    
                         <h1>Congrats ! You found them all !</h1>
-                        <h2> Your time is: </h2>
+                        <h2> Your time is {getMinutes}:{getSeconds}</h2>
 
                         <div className='user-input'>
                             <p> Enter your name below to join the rankings: </p>
-                            <input type='text'></input>
-                            <button type='submit'>Submit</button>
+                            <input type='text' value={input} onChange={(e) => setInput(e.target.value)}/>
+                            <button type='submit' onClick={(e) => handleSubmit(e)}>Submit</button>
                         </div>
 
                         <h3>Here are our high scores so far:</h3>
                         
                         <div className='modal-highscores'>
-                            
+                            {
+                                sortedUsers.map((user, index) => {
+                                    const getUserSeconds = `0${(user.time % 60)}`.slice(-2)
+                                    const userMinutes = `${Math.floor(user.time / 60)}`
+                                    const getUserMinutes = `0${userMinutes % 60}`.slice(-2)
+                                    
+                                    return (
+                                        <li className='highscores-list' key={index}>
+                                            <ul>
+                                                <div className='user-highscores'>
+                                                <p>{user.name}</p> &nbsp;
+                                                <p>{getUserMinutes}:{getUserSeconds}</p>
+                                                
+                                                </div>
+                                            </ul>
+                                        </li>
+                                    )
+                                })
+                            }
 
                         </div>
                         
---
+++ src/components/Stopwatch.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState, useRef} from 'react'
+
+
+const Stopwatch = ({ setTime, stopTimer }) => {
+    const [timer, setTimer] = useState(0)
+    const [isActive, setIsActive] = useState(false)
+    const [isPaused, setIsPaused] = useState(false)
+    const countRef = useRef(null)
+
+    const handlePause = () => {
+        clearInterval(countRef.current)
+        setIsPaused(false)
+    }
+
+    const handleStart = () => {
+        setIsActive(true)
+        setIsPaused(true)
+        countRef.current = setInterval(() => {
+            setTimer((timer) => timer + 1)
+        }, 1000)
+    }
+
+    useEffect(() => {
+        handleStart()
+        return () => {
+            handlePause()
+        }
+    }, [])
+    
+    useEffect(() => {
+        if (stopTimer) {
+            handlePause()
+            setTime(timer)
+        }
+    })
+    const getSeconds = `0${(timer % 60)}`.slice(-2)
+    
+    const minutes = `${Math.floor(timer / 60)}`
+    const getMinutes = `0${minutes % 60}`.slice(-2)
+    return (
+        <div className='stopwatch-card' style={{ fontSize: 50, marginTop: 20}}>
+            <p>{getMinutes}:{getSeconds}</p> {/* here we will show timer */}
+        </div>
+    );
+
+
+}
+
+export default Stopwatch
